bd8a5adf33d3449c08a2b6f8ff0827c1a3d616ed
Saidul147 <sidul.islam.146@gmail.com>
2025-09-29 17:35:40 +0600

    upcoming calender added

---
+++ components/ClientComponents/Forecast/UpcomingCalender.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { ChevronDownIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+
+
+
+export default function UpcomingCalender() {
+
+    const [open, setOpen] = React.useState(false)
+    const [date, setDate] = React.useState<Date | undefined>(undefined)
+
+    return (
+        <div>
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button
+                        variant="outline"
+                        id="date"
+                        className="md:py-6 py-4 shadow-sm rounded-[4px] px-3 w-[252px] cursor-pointer focus-visible:border-none focus-visible:ring-none focus-visible:ring-[0px] border-0  bg-white text-[#4A4C56] md:text-base text-sm leading-[130%] font-normal flex justify-between"
+                    >
+                        {date ? date.toLocaleDateString() : "Select date"}
+                        <ChevronDownIcon />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto overflow-hidden p-0" align="end">
+                    <Calendar
+                        mode="range"
+                        defaultMonth={date}
+                        numberOfMonths={1}
+                        className="rounded-lg border shadow-sm"
+                    />
+                </PopoverContent>
+            </Popover>
+        </div>
+    )
+}
--- components/ClientComponents/Forecast/UpcomingWeather.tsx
+++ components/ClientComponents/Forecast/UpcomingWeather.tsx
@@ -1,6 +1,6 @@
+"use client"
 import React from 'react';
 import ForecastTable from './ForecastTable';
-
 import {
     Select,
     SelectContent,
@@ -8,6 +8,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import UpcomingCalender from './UpcomingCalender';
 
 const selectItem = [
     { value: "3 days", label: "3 Days" },
@@ -15,8 +16,12 @@ const selectItem = [
     { value: "10 days", label: "10 Days" }
 ]
 
-export default function WeatherForecast() {
 
+
+
+
+
+export default function WeatherForecast() {
 
 
 
@@ -24,7 +29,7 @@ export default function WeatherForecast() {
         <div className='maxContainer'>
             <div className='flex items-center justify-between'>
                 <h2 className='text-[#1D1F2C] lg:leading-[32px] md:text-[28px] text-2xl font-bold leading-[130%]'>Upcoming Weather Forecast</h2>
-                <div>
+                <div className='flex gap-4 '>
                     <div>
                         <Select>
                             <SelectTrigger className="md:py-6 py-4 shadow-sm rounded-[4px] px-3 w-[128px] cursor-pointer focus-visible:border-none focus-visible:ring-none focus-visible:ring-[0px] border-0  bg-white text-[#4A4C56] md:text-base text-sm leading-[130%] font-normal ">
@@ -32,13 +37,13 @@ export default function WeatherForecast() {
                             </SelectTrigger>
                             <SelectContent>
                                 {selectItem?.map((item) => (
-                                    <SelectItem className='cursor-pointer' value={item?.value} >{item?.label}</SelectItem>
+                                    <SelectItem key={item?.label} className='cursor-pointer' value={item?.value} >{item?.label}</SelectItem>
                                 ))}
                             </SelectContent>
                         </Select>
                     </div>
-                    <div>
-                        
+                    <div >
+                        <UpcomingCalender/>
                     </div>
 
                 </div>
