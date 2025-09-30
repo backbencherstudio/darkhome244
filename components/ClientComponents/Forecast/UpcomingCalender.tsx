66133f46b8555d2e3c1ab9e1d7c9c9e0fc91c0f0
Saidul147 <sidul.islam.146@gmail.com>
2025-09-30 14:45:33 +0600

    calender icon added

--- components/ClientComponents/Forecast/UpcomingCalender.tsx
+++ components/ClientComponents/Forecast/UpcomingCalender.tsx
@@ -7,6 +7,7 @@ import {
 import { ChevronDownIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
+import CalenderIcon from '@/components/Icons/CalenderIcon';
 
 
 
@@ -25,7 +26,7 @@ export default function UpcomingCalender() {
                         className="md:py-6 py-4 shadow-sm rounded-[4px] px-3 w-[252px] cursor-pointer focus-visible:border-none focus-visible:ring-none focus-visible:ring-[0px] border-0  bg-white text-[#4A4C56] md:text-base text-sm leading-[130%] font-normal flex justify-between"
                     >
                         {date ? date.toLocaleDateString() : "View Forecast"}
-                        {/* <ChevronDownIcon /> */}
+                        <CalenderIcon />
                     </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-auto overflow-hidden p-0" align="end">
