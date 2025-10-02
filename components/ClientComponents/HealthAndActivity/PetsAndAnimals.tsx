6b13419496ba88c30ec040683739725cb7b475e3
Saidul147 <sidul.islam.146@gmail.com>
2025-10-02 15:47:14 +0600

    every tab component set on health and activity

--- components/ClientComponents/HealthAndActivity/PetsAndAnimals.tsx
+++ components/ClientComponents/HealthAndActivity/PetsAndAnimals.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import HealthAndActivityCard from './HealthAndActivityCard';
+const RSS_FEEDS = {
+  latestNews: 'https://feeds.feedburner.com/EsoTopNews',
+  weatherNews: 'https://feeds.feedburner.com/EsoTopNews',
+  extremeWeather: 'https://feeds.npr.org/1165/rss.xml'
+};
+
 
 export default function PetsAndAnimals() {
   return (
-    <div>PetsAndAnimals</div>
+    <div>
+     <HealthAndActivityCard RSS_FEEDS={RSS_FEEDS} headerTitle="Latest Pets And Animals News" />
+    </div>
   )
 }
