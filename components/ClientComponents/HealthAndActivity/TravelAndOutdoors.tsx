3bde8837b84e7723db8cf1492fdceba260684d4b
Saidul147 <sidul.islam.146@gmail.com>
2025-10-02 17:02:15 +0600

    travel and outdoor added

--- components/ClientComponents/HealthAndActivity/TravelAndOutdoors.tsx
+++ components/ClientComponents/HealthAndActivity/TravelAndOutdoors.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import HealthAndActivityCard from './HealthAndActivityCard';
 const RSS_FEEDS = {
-  latestNews: 'https://feeds.feedburner.com/EsoTopNews',
-  weatherNews: 'https://feeds.feedburner.com/EsoTopNews',
-  extremeWeather: 'https://feeds.npr.org/1165/rss.xml'
+  latestNews: 'https://www.outdoorlife.com/feed/',
+  weatherNews: 'https://www.outdoorlife.com/feed/',
+  extremeWeather: 'https://www.outdoorhub.com/feed/'
 };
 
 
 export default function TravelAndOutdoors() {
   return (
     <div>
-      <HealthAndActivityCard RSS_FEEDS={RSS_FEEDS} headerTitle="Latest Recipes News" />
+      <HealthAndActivityCard RSS_FEEDS={RSS_FEEDS} headerTitle="Latest Travel & Outdoors News" />
     </div>
   )
 }
