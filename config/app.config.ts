780d850269d9bd432435152d15932231adb586f6
Saidul147 <sidul.islam.146@gmail.com>
2025-11-06 13:00:21 +0600

    favicon has displayed

--- config/app.config.ts
+++ config/app.config.ts
@@ -6,11 +6,11 @@ export const AppConfig = () => ({
   app: {
     // server endpoint
     url: URL,
-    name: "app",
-    slogan: "app",
+    name: "weatherTides",
+    slogan: "weatherTides",
     meta: {
-      description: "app",
-      keywords: "app",
+      description: "weatherTides",
+      keywords: "weatherTides",
     },
 
     // api endpoint
